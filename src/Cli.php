d2205ca237e2fe6b8f43dc8387b7613ccd1bd465
Maksim Litvinov <litvinovmksm@gmail.com>
2019-11-21 19:40:12 +0300

    Add Makefile and file Cli.php

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+namespace BrainGames\Cli;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
+    line('Welcome to brain games!');
+    $name = prompt('May I have your name?');
+    line('Hello %s', $name);
+    return;
+}
